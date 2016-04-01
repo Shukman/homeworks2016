e7345b29716dafa7159d3c7e7fe5b59bd2739d42
Vyacheslav <Shukmanv@inbox.ru>
2016-04-01 22:11:28 +0300

    added language version

--- homeworks/src/lesson160331/Task01.java
+++ homeworks/src/lesson160331/Task01.java
@@ -4,43 +4,43 @@ public class Task01 {
 	
 	public static void main(String[] args) {
 		
-//пример 1 вариант 1		
+//example 1.1		
 		double x = 28E3;
-		System.out.println("ќклад по часам: " + x + "р");
+		System.out.println("pay by the hour: " + x + "p");
 
 		double y = 1E3;
-		System.out.println("ƒоплата за питание: " + y + "р");
+		System.out.println("extra charge for food: " + y + "p");
 
 		double c = 1145.22;
-		System.out.println("ƒоплата за ночные часы: " + c + "р");
+		System.out.println("surcharge for night: " + c + "p");
 
 		double l = x + y + c;
-		System.out.println("ќбщий облагаемый доход: " + l + "р");
+		System.out.println("the total taxable income: " + l + "p");
 		
 		double p = (l * 13 / 100);
-		System.out.println("¬сего удержано: " + p + "р");
+		System.out.println("total retained: " + p + "p");
 		
 		double z = l - p;
-		System.out.println("  выплате: " + z + "р");
+		System.out.println("payoff: " + z + "p");
 		
 		System.out.println();
-//пример 1 вариант 2
+//example 1.2
 		double z1 = l - ((x + y + c) * 0.13);
-		System.out.println("  выплате: " + z1 + "р"); 
+		System.out.println("payoff: " + z1 + "p"); 
 		
 		System.out.println();
-//пример 2 вариант 1		
+//example 2.1		
 		int ozp = 30_000;
 		int nalog = (ozp * 13 / 100);
 		int zp = ozp - nalog;
-		System.out.println("обща€ заработна€ плата - " + ozp);
-		System.out.println("вычитаемый налог - " + nalog);
-		System.out.println("заработна€ плата к выплате - " + zp);
+		System.out.println("pay by the hour - " + ozp);
+		System.out.println("total retained - " + nalog);
+		System.out.println("payoff - " + zp);
 		
 		System.out.println();
-//пример 2 вариант 2
+//example 1.2
 		int zp1 = ozp - (ozp * 13 / 100);
-		System.out.println("заработна€ плата к выплате - " + zp1);
+		System.out.println("payoff - " + zp1);
 	}
 
 }
