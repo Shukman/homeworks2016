848f23041c54130ea9e3f9f30d4d3668da95f313
Vyacheslav <Shukmanv@inbox.ru>
2016-04-08 00:50:54 +0300

    added homework160407

---
+++ homeworks/src/homework160407/DoWhileExample.java
@@ -0,0 +1,15 @@
+package homework160407;
+
+public class DoWhileExample {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		do {
+			i = i + 1;
+			System.out.println(i);
+		} while (i < 10);
+	}
+
+}
